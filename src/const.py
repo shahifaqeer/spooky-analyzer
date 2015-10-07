f878247ff51c9e7bb1e96c7a12e5fb345889542e
Grover <sgrover@cs.princeton.edu>
2015-10-07 17:09:56 -0400

    change folder

--- src/const.py
+++ src/const.py
@@ -1,10 +1,10 @@
 # filename (use dates) in data/ folder
-fname = "20150422_unbiased.csv"
+fname = "20150921_prelim_exp2.csv"
 #fname = "20150412_biased.csv"
-fdate = "20150422"
+fdate = "20150921"
 
 # massive concat data (if in use)
-final_concat_fdate = "20150422"
+final_concat_fdate = "20150921"
 
 # parameters
 # intervention for case detection
